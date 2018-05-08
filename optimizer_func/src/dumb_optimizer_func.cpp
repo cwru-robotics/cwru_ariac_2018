79410d19817f90e06120b71f4d7fe4a0d897bed7
Greg Lee <leegs@case.edu>
2018-05-08 17:28:21 -0400

    Created a "dumb" and "smart" service that only differ by a single function.  Added the building to the CMakeLists.txt.
    For the "dumb" optimizer:
    rosrun optimizer_func dumb_optimizer
    
    For the "smart" optimizer:
    rosrun optimizer_func decision_optimizer

--- optimizer_func/src/dumb_optimizer_func.cpp
+++ optimizer_func/src/dumb_optimizer_func.cpp
@@ -1,13 +1,6 @@
-#include <ros/ros.h>
-#include <optimizer_func/optimizer_msgs.h>
+#include "optimizer_func/optimizer_func.h"
+#include "optimizer_func/helper_funcs.h"
 
-#include <osrf_gear/Shipment.h>
-#include <osrf_gear/Order.h>
-
-osrf_gear::Order current, priority;
-ros::Time current_order_recvd, priority_order_recvd;
-
-char *shipment_names[] = {"no_active_shipment", "order_0_shipment_0", "order_0_shipment_1", "order_1_shipment_0", "order_1_shipment_1"};
 
 int case_number = 0;
 
@@ -22,6 +15,10 @@ bool optimize_shipments(optimizer_func::optimizer_msgs::Request  &req,
        (req.reposition.products.size() == 0)) ||	\
       (req.giving_up == optimizer_func::optimizer_msgsRequest::GIVING_UP)) {
 
+    // Set the decision to ship with label and start loading next shipment
+    res.decision = optimizer_func::optimizer_msgsResponse::PRIORITY_LOAD_NEXT;
+
+    // What is the next shipment
     switch (case_number) {
     case 0:
       break;
@@ -51,9 +48,12 @@ bool optimize_shipments(optimizer_func::optimizer_msgs::Request  &req,
       break;
     }	
     
-    res.decision = optimizer_func::optimizer_msgsResponse::PRIORITY_LOAD_NEXT;
   } else {
-  
+
+    // Set the decision to keep loading the current shipment
+    res.decision = optimizer_func::optimizer_msgsResponse::USE_CURRENT_BOX;
+
+    // What is the current shipment being loaded
     switch (case_number) {
     case 0:
       if (current.shipments.size() > 0) {
@@ -78,41 +78,7 @@ bool optimize_shipments(optimizer_func::optimizer_msgs::Request  &req,
     default:
       res.shipment = empty_shipment;
     }
-    res.decision = optimizer_func::optimizer_msgsResponse::USE_CURRENT_BOX;
   }
   return true;
 }
 
-void orderCallback(const osrf_gear::Order::ConstPtr& msg) {
-
-  if (! msg->order_id.compare("order_0")) {
-    current_order_recvd.now();
-    current = *msg;
-  } else if (!msg->order_id.compare("order_0_update_0")) {
-    current = *msg;
-  } else if (!msg->order_id.compare("order_1")) {
-    priority_order_recvd.now();
-    priority = *msg;
-  } else if (!msg->order_id.compare("order_1_update_0")) {
-    priority = *msg;
-  }
-}
-
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "dumb_optimizer");
-  ros::NodeHandle n;
-
-  current.shipments.resize(1);
-  current.shipments[0].shipment_type.append(shipment_names[0]);
-  
-  ros::ServiceServer service = n.advertiseService("optimizer", optimize_shipments);
-  ROS_INFO("Ready to respond (dumbly) to optimization requests.");
-
-  ros::Subscriber sub = n.subscribe("ariac/orders", 10, orderCallback);
-  
-  ros::spin();
-
-  return 0;
-}
