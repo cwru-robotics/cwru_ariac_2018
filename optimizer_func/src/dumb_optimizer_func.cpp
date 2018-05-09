ab7c6466331ec9d12b419a0e6b92bb68e6e33cc5
Greg Lee <leegs@case.edu>
2018-05-09 15:27:11 -0400

    Fixed dumb behavior for Q2 query.

--- optimizer_func/src/dumb_optimizer_func.cpp
+++ optimizer_func/src/dumb_optimizer_func.cpp
@@ -28,14 +28,15 @@ bool optimize_shipments(optimizer_func::optimizer_msgs::Request  &req,
   } else if(req.inspection_site == optimizer_func::optimizer_msgsRequest::Q2_STATION) {
     ROS_INFO("Request from Q2");
     ROS_INFO("Always advancing box (showing next shipment).");
+    res.decision = optimizer_func::optimizer_msgsResponse::PRIORITY_LOAD_NEXT;
     // if (req.giving_up == optimizer_func::optimizer_msgsRequest::GIVING_UP) {
-    if(shipment_queue.shipments.size() > shipment_queue_indx) {
-      ROS_INFO("Sending next shipment; queue size %i: indx: %i", (int) shipment_queue.shipments.size(), shipment_queue_indx);
-      res.shipment = shipment_queue.shipments[shipment_queue_indx];
-    } else {
-      ROS_INFO("Sending empty shipment; queue size %i: indx: %i", (int) shipment_queue.shipments.size(), shipment_queue_indx);
-      res.shipment = empty_shipment;
-    } 
+    // if(shipment_queue.shipments.size() > shipment_queue_indx) {
+    //   ROS_INFO("Sending next shipment; queue size %i: indx: %i", (int) shipment_queue.shipments.size(), shipment_queue_indx);
+    //   res.shipment = shipment_queue.shipments[shipment_queue_indx];
+    // } else {
+    //   ROS_INFO("Sending empty shipment; queue size %i: indx: %i", (int) shipment_queue.shipments.size(), shipment_queue_indx);
+    //   res.shipment = empty_shipment;
+    // } 
     // }
   } else {
     ROS_ERROR("Request did not come from Q1 or Q2!!");
